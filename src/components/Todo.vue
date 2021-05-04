17331b57c5580c91fdd2950985e6815abeae4c73
takuyatechexpert <lp4546.sg.abc@gmail.com>
2021-05-04 18:16:50 +0900

    fix todo page

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,50 +1,96 @@
 <template>
-  <div>
-    <div class="text-center text-4xl mt-1">TODO</div>
-
-    <div class="text-center mt-4">
+  <page title="TODO">
+    <div class="text-center mt-4 h-8">
       <input
         type="text"
         v-model="formData.todo"
-        class="border border-black rounded-md h-8 p-4">
-      <button class="mx-3 h-8 border border-black rounded-md px-2" @click="onSend()">send</button>
+        class="border border-black w-4/12 rounded-md h-full px-2"
+      />
+      <button
+        class="mx-3 h-full btn hover:bg-pink-500 focus:outline-none focus:ring focus:ring-pink-400"
+        @click="onSend()">
+        Send
+      </button>
     </div>
 
-    <div class="text-center mt-4 text-xl">
-      <ul>
-        <li v-for="(todo, index) in todos" :key="index">{{ todo }}</li>
+    <div class="mt-4 text-xl overflow-auto h-200">
+      <ul class="w-2/5 m-auto">
+        <li v-for="(todo, index) in todos"
+          :key="index">
+          {{ todo }}
+          <button class="bg-blue-300 btn ml-3 mb-2 hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300"
+            @click="onDelete(index)">
+            Done
+          </button>
+        </li>
       </ul>
     </div>
-  </div>
+  </page>
 </template>
 
 <script lang='ts'>
-import vue, { PropType } from 'vue'
+import vue from 'vue'
 
 interface Data {
   formData: {
-    todo: string,
-  }
+    todo: string;
+  };
 
-  todos: [],
+  todos: string[];
 }
 
 export default vue.extend({
+
   data(): Data {
     return {
       formData: {
-        todo: ''
+        todo: "",
       },
 
-      todos: []
+      todos: [],
+    };
+  },
+
+  watch: {
+    formData: {
+      handler() {
+        let comFlag = true
+
+        window.addEventListener('compositionstart', () => {
+          comFlag = false
+        })
+
+        window.addEventListener('keydown', (e) => {
+           if (e.key === 'Enter' && comFlag) {
+            this.onSend()
+          }
+        })
+
+        window.addEventListener('compositionend', () => {
+          comFlag = true
+        })
+      },
+      deep: true,
     }
   },
 
   methods: {
     onSend(): void {
-      this.todos.push(this.formData.todo)
-      this.formData.todo = ''
-    }
-  }
-})
+      if (this.formData.todo === "") return;
+
+      this.todos.push(this.formData.todo);
+      this.formData.todo = "";
+    },
+
+    onDelete(index: number): void {
+      this.todos.splice(index, 1);
+    },
+  },
+});
 </script>
+
+<style scoped>
+.h-200 {
+  height: 800px;
+}
+</style>>
